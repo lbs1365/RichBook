0fd26ccb9bbdfc5c9e310d50f0c1e17872108650
402-d3 <402-d3@402-d3-PC>
2015-06-15 16:20:38 +0900

    new project

--- richbook/src/main/java/com/ch/richbook/Good.java
+++
@@ -1,7 +0,0 @@
-package com.ch.richbook;
-
-public class Good {
-	String hi;
-	int a = 13111;
-	String hello;
-}
--- richbook/src/main/java/com/ch/richbook/Hi.java
+++
@@ -1,6 +0,0 @@
-package com.ch.richbook;
-
-public class Hi {
-	String good;
-	String Kin;
-}
